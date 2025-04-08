efec2c56421d8ef0dad58e504fd1d77767ad69c2
NaeemK1188 <naeemalbirkdar@aol.com>
2025-04-07 17:26:54 -0700

    testing end-point

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,9 +9,30 @@ function App()
 
   const [msg, setmsg] =useState("");
 
+  //useEffect (default and best for side-effects)
+  // Recommended for:Fetching on mount React lifecycle consistency
   useEffect(() => {
-    
+    async function load()
+    {
+      const res = await fetch("/api/test");
+      if (!res.ok)
+      {
+        throw new Error(`fetch error ${res.status}`)
+      }
 
+      //const text = await res.json();
+      // we are having error when using .json() with plain text
+      // because .json() tries to parse the response as JSON (expects something like {"message": "hello world"})
+      // But it sees "H" (from Hello world) and throws SyntaxError: Unexpected token H in JSON at position 0
+      // If your server is sending plain text (res.send("hello world")), use .text() instead:
+      //If you want to stick with res.json() in the frontend, then your backend needs to send valid JSON, like this:
+      //res.json({ message: "hello world" }); // valid JSON
+      const text = await res.text();
+      console.log(text);
+      setmsg(text);
+    }
+   
+    load();
   }, [])
 
 
@@ -19,6 +40,7 @@ function App()
   return (
     <>
       <h2 className='text-2xl underline font-bold'>Im using tailwind</h2>
+      <h1>{msg}</h1>
     </>
   )
 }
